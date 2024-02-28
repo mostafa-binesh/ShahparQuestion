8b5f5f9d793b18c80d0dc1fae063877985ea2ba9
Mostafa <ubirockteam@gmail.com>
2024-02-28 22:48:46 +0330

    feat: add time log processor service and interface

---
+++ ShahparQuestion/Interfaces/ITimeLogProcessorService.cs
@@ -0,0 +1,16 @@
+﻿using ShahparQuestion.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShahparQuestion.Interfaces
+{
+    public interface ITimeLogProcessorService
+    {
+        IEnumerable<DailySummary> ProcessLogs(IEnumerable<EmployeeLog> records);
+
+    }
+}
