58e0e8824f0ea48f8eb26e6dec6001a53c2ad952
Mostafa <ubirockteam@gmail.com>
2024-02-28 22:44:38 +0330

    feat: add viewModels

---
+++ ShahparQuestion/ViewModels/ValidationResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShahparQuestion.ViewModels
+{
+    public class ValidationResult
+    {
+        public bool IsValid => !Errors.Any();
+        public List<string> Errors { get; set; } = new List<string>();
+
+    }
+}
