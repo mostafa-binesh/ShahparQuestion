c78837b11f2e613815e5b2d5fff712c5441caece
Mostafa <ubirockteam@gmail.com>
2024-02-28 22:50:57 +0330

    feat: add validation interface and service

---
+++ ShahparQuestion/Interfaces/IEmployeeRecordsValidationService.cs
@@ -0,0 +1,14 @@
+﻿using ShahparQuestion.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShahparQuestion.Interfaces
+{
+    public interface IEmployeeRecordsValidationService
+    {
+        ValidationResult Validate(IEnumerable<EmployeeLog> record);
+    }
+}
---
+++ ShahparQuestion/Services/EmployeeRecordsValidationService.cs
@@ -0,0 +1,45 @@
+﻿using ShahparQuestion.Interfaces;
+using ShahparQuestion.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    internal class EmployeeRecordsValidationService : IEmployeeRecordsValidationService
+    {
+        public ValidationResult Validate(IEnumerable<EmployeeLog> records)
+        {
+            var result = new ValidationResult();
+
+            // Validate that each employeeId is 9 digits
+            foreach (var record in records)
+            {
+                if (record.EmployeeID.Length != 9 || !record.EmployeeID.All(char.IsDigit))
+                {
+                    //results.Add(new ValidationResult(false, ));
+                    result.Errors.Add($"آیدی کارمند '{record.EmployeeID}' دقیقا باید به صورت عدد و 9 رقمی باشد.");
+                }
+            }
+
+            // Validate that there is no employeeId with two names
+            var employeeIdNamePairs = records
+                .GroupBy(r => r.EmployeeID)
+                .Select(g => new { EmployeeID = g.Key, Names = g.Select(x => x.Name).Distinct() });
+
+            foreach (var pair in employeeIdNamePairs)
+            {
+                if (pair.Names.Count() > 1)
+                {
+                    result.Errors.Add($"کارمند با آیدی '{pair.EmployeeID}' با نام های متفاوت وجود دارد.");
+                }
+            }
+            return result;
+        }
+    }
+
+}
