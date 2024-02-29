7a04c2a633a9da561621bc674a6cf1269adf93b9
Mostafa <ubirockteam@gmail.com>
2024-02-29 23:08:12 +0330

    improve reading file functionnality

--- BusinessLayer/Services/TextFileEmployeeLogRetrieverService.cs
+++ BusinessLayer/Services/TextFileEmployeeLogRetrieverService.cs
@@ -6,12 +6,10 @@ namespace BusinessLayer.Services
 {
     public class TextFileEmployeeLogRetrieverService : ITextFileEmployeeLogRetrieverService
     {
-        //private string? _filePath;
         private IFormFile _file;
 
         public void SetFile(IFormFile file)
         {
-            //_filePath = filePath;
             _file = file;
         }
 
@@ -19,45 +17,37 @@ namespace BusinessLayer.Services
         {
             var result = new EmployeeLogParsingResult();
 
-            //if (string.IsNullOrEmpty(_filePath))
-            //{
-            //    throw new Exception(message: "filePath should be provided by using SetFilePath function");
-            //}
             if (_file == null || _file.Length == 0)
             {
                 throw new Exception("A file must be provided.");
             }
 
-            //var lines = File.ReadAllLines(_filePath);
-
-            //foreach (var line in lines)
-            //{
+            // read the file line by line
             using (var stream = _file.OpenReadStream())
             using (var reader = new StreamReader(stream))
             {
                 string? line;
                 while ((line = await reader.ReadLineAsync()) != null)
                 {
-                    var parts = line.Split('\t');
-                    if (parts.Length < 3)
+                    // split the data by space and \t
+                    string[] parts = line.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    if (parts.Length < 4)
                     {
                         result.Errors.Add($"داده ی خط {line} از فرمت درست پیروی نمی کند");
-                        //continue;
+                        continue;
                     }
 
+                    // extract the information from seperated parts
                     var employeeId = parts[0].Trim();
                     var name = parts[1].Trim();
-                    var dateTimePart = parts[2].Trim();
+                    var dateTimePart = parts[2].Trim() + " " + parts[3].Trim();
 
                     // Attempt to parse the DateTime part
                     if (!DateTime.TryParse(dateTimePart, out var logDateTime))
                     {
                         result.Errors.Add($"تاریخ و زمان {dateTimePart} در خط {line} نامعتبر است");
-                        //continue;
                     }
-                    //var groupedByEmployeeAndDate = result.Records
-                    //        .GroupBy(r => new { r.EmployeeID, r.LogDateTime.Date })
-                    //.ToList();
 
                     var record = new EmployeeLog
                     {
